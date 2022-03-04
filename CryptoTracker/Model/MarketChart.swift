a2155cb3ac095645462da01f07a2fa138bcd4d9a
Kevin <binxkevin@gmx.de>
2022-03-04 21:45:07 +0100

    Create MarketChart model

---
+++ CryptoTracker/Model/MarketChart.swift
@@ -0,0 +1,14 @@
+//
+//  MarketChart.swift
+//  CryptoTracker
+//
+//  Created by Kevin Hoàng on 04.03.22.
+//
+
+import Foundation
+
+// https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=daily
+
+struct MarketChart {
+    let prices: [[Double]]
+}
