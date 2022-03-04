07bc4a76ba88fe743faf266d0032be90c89c70c3
Kevin <binxkevin@gmx.de>
2022-03-04 15:52:50 +0100

    Create Formatters

---
+++ CryptoTracker/Extension/Double.swift
@@ -0,0 +1,49 @@
+//
+//  Double.swift
+//  CryptoTracker
+//
+//  Created by Kevin Hoàng on 04.03.22.
+//
+
+import Foundation
+
+
+extension Double {
+    
+    /// Converts a Double into a Currency with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converts a Double into a Currency as a String with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// ```
+    func asCurrency() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter.string(from: number) ?? "$0.00"
+    }
+    
+    /// Converts a Double into string representation
+    /// ```
+    /// Convert 1.2345 to "1.23"
+    /// ```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    /// Converts a Double into string representation with percent symbol
+    /// ```
+    /// Convert 1.2345 to "1.23%"
+    func asPercentString() -> String {
+        return asNumberString() + "%"
+    }
+}
