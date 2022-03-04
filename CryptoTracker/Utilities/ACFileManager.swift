9f3624ea842fe7cfc4183033d4b27e373bb1e9fe
Kevin <binxkevin@gmx.de>
2022-03-04 15:01:01 +0100

    Create ACFileManager

---
+++ CryptoTracker/Utilities/ACFileManager.swift
@@ -0,0 +1,66 @@
+//
+//  ACFileManager.swift
+//  CryptoTracker
+//
+//  Created by Kevin Hoàng on 04.03.22.
+//
+
+import Foundation
+import UIKit
+
+class ACFileManager {
+    
+    static let shared = ACFileManager()
+    private let folderName = "cached_images"
+    
+    func saveImage(image: UIImage, imageName: String) {
+        createFolderIfNeeded(folderName: folderName)
+        
+        guard let data = image.pngData(),
+              let url = getURLForImage(imageName: imageName, folderName: folderName)
+        else { return }
+        
+        do {
+            try data.write(to: url)
+        } catch {
+            print("Error saving image. ImageName: \(imageName). \(error)")
+        }
+    }
+    
+    func getImage(imageName: String) -> UIImage? {
+        guard let url = getURLForImage(imageName: imageName, folderName: folderName),
+              FileManager.default.fileExists(atPath: url.path)
+        else {
+            return nil
+        }
+        
+        return UIImage(contentsOfFile: url.path)
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(folderName: folderName) else { return }
+        
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    private func getURLForFolder(folderName: String) -> URL? {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return url.appendingPathComponent(folderName)
+    }
+    
+    private func getURLForImage(imageName: String, folderName: String) -> URL? {
+        guard let folderURL = getURLForFolder(folderName: folderName) else {
+            return nil
+        }
+        
+        return folderURL.appendingPathComponent(imageName + ".png")
+    }
+}
