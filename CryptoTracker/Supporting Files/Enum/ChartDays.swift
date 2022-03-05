09c845f3b440a7d63bd897c6cc312b3577809feb
Kevin <binxkevin@gmx.de>
2022-03-05 13:34:20 +0100

    Create ChartDays

---
+++ CryptoTracker/Supporting Files/Enum/ChartDays.swift
@@ -0,0 +1,18 @@
+//
+//  ChartDays.swift
+//  CryptoTracker
+//
+//  Created by Kevin Hoàng on 05.03.22.
+//
+
+import Foundation
+
+enum ChartDays: String, CaseIterable {
+    case one = "1"
+    case seven = "7"
+    case fourteen = "14"
+    case thirty = "30"
+    case ninety = "90"
+    case year = "365"
+    case max = "max"
+}
