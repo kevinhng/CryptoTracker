4f172547615e656f963f50b77f2f2417fdbddf1a
Kevin <binxkevin@gmx.de>
2022-03-05 12:53:04 +0100

    Update MarketChart model

--- CryptoTracker/Model/MarketChart.swift
+++ CryptoTracker/Model/MarketChart.swift
@@ -10,5 +10,13 @@ import Foundation
 // https://api.coingecko.com/api/v3/coins/ethereum/market_chart?vs_currency=usd&days=30&interval=daily
 
 struct MarketChart: Codable {
-    let prices: [[Double]]
+    let chart: [[Double]]
+    
+    var timestamps: [Double] {
+        return chart.map { return $0[0] }
+    }
+    
+    var prices: [Double] {
+        return chart.map { return $0[1] }
+    }
 }
