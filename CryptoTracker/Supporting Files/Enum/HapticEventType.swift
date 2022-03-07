4223b089ef91c047e7bdca4ba57d2794b309eea0
Kevin <binxkevin@gmx.de>
2022-03-07 16:22:36 +0100

    Create HapticEventType

---
+++ CryptoTracker/Supporting Files/Enum/HapticEventType.swift
@@ -0,0 +1,14 @@
+//
+//  HapticEventType.swift
+//  CryptoTracker
+//
+//  Created by Kevin Hoàng on 07.03.22.
+//
+
+import Foundation
+
+enum HapticEventType {
+    case began
+    case changed
+    case ended
+}
