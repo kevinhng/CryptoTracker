038e80517bb586c4987f7bff4fb958c7c566e873
Kevin <binxkevin@gmx.de>
2022-03-07 16:25:05 +0100

    Recode and fix HapticsManager

--- CryptoTracker/Utilities/HapticsManager.swift
+++ CryptoTracker/Utilities/HapticsManager.swift
@@ -8,12 +8,13 @@
 import CoreHaptics
 import Foundation
 
-enum HapticsManager {
+class HapticsManager {
     
-    case began,changed,ended
+    static let instance = HapticsManager()
     
-    private var engine: CHHapticEngine? {
-        let engine: CHHapticEngine
+    private var engine: CHHapticEngine
+    
+    init?() {
         guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
             print("Device does not support CoreHaptics")
             return nil
@@ -22,47 +23,54 @@ enum HapticsManager {
         do {
             engine = try CHHapticEngine()
             try engine.start()
-            return engine
         } catch {
             print("There was an error creating the engine: \(error.localizedDescription)")
             return nil
-            
+        }
+        
+        engine.stoppedHandler = { reason in
+            print("The engine stopped: \(reason)")
+        }
+
+        // If something goes wrong, attempt to restart the engine immediately
+        engine.resetHandler = { [weak self] in
+            print("The engine reset")
+
+            do {
+                try self?.engine.start()
+            } catch {
+                print("Failed to restart the engine: \(error)")
+            }
         }
     }
     
-    func playFeedback() {
-        guard let engine = engine else {
-            return
-        }
+    func playFeedback(for eventType: HapticEventType) {
         
         var intensity: CHHapticEventParameter
         var sharpness: CHHapticEventParameter
-        var relativeTime: TimeInterval = 1
         
-        switch self {
+        switch eventType {
         case .began:
             intensity =  CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
             sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-            relativeTime = 0.5
         case .changed:
-            intensity =  CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-            sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-            relativeTime = 0.5
+            intensity =  CHHapticEventParameter(parameterID: .hapticIntensity, value: 0.4)
+            sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.3)
         case .ended:
             intensity =  CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
-            sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
-            relativeTime = 0.5
+            sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0.5)
         }
                 
-        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: relativeTime)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
         
         do {
+            try engine.start()
             let pattern = try CHHapticPattern(events: [event], parameters: [])
             let player = try engine.makePlayer(with: pattern)
-            try player.start(atTime: 0)
+            try player.start(atTime: CHHapticTimeImmediate)
             print("playing feedback")
         } catch {
-            print("Failed to play pattern: \(error).")
+            print("Failed to play pattern: \(error.localizedDescription).")
         }
     }
 }
